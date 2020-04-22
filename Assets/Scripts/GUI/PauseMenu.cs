57d91c4bb5386355278e7ecaaa2e5a96197869ca
Thabb <49516328+Thabb@users.noreply.github.com>
2020-04-23 00:29:43 +0200

    ADD: First take on a menu

---
+++ Assets/Scripts/GUI/PauseMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    public void ResumeButton()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
