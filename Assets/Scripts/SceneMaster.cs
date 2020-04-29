44ea0763907034b2516c0a381bc4d28b471a38a6
Thabb <49516328+Thabb@users.noreply.github.com>
2020-04-29 13:35:22 +0200

    ADD: Basic functionality to stack GUI Levels

--- Assets/Scripts/SceneMaster.cs
+++ Assets/Scripts/SceneMaster.cs
@@ -5,8 +5,21 @@ using UnityEngine;
 public class SceneMaster : MonoBehaviour
 {
     public GameObject pauseMenu;
+
+    private List<GUIElement> _openGUIElements = new List<GUIElement>();
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape)) pauseMenu.SetActive(true);
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            if (_openGUIElements.Count != 0)
+            {
+                _openGUIElements[_openGUIElements.Count - 1].gameObject.SetActive(false);
+                _openGUIElements.RemoveAt(_openGUIElements.Count - 1);
+            } else
+            {
+                pauseMenu.SetActive(true);
+                _openGUIElements.Add(pauseMenu.GetComponent<GUIElement>());
+            }
+        }
     }
 }
